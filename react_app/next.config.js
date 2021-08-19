6bf25a840be2dafb25d6b8eb497ec4095d41c3bd
matthew <matthewbudiman@outlook.com>
2021-08-18 21:25:44 -0500

    Now?

--- react_app/next.config.js
+++ react_app/next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     EXPRESS_URL: process.env.EXPRESS_URL,
+    EXPRESS_SERVICE_PORT: process.env.EXPRESS_SERVICE_PORT,
+    EXPRESS_SERVICE_HOST: process.env.EXPRESS_SERVICE_HOST,
   },
 };
